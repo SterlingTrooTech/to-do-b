0643e7318f25c074012c557971b94046d03374ad
SpGoku <sterling.parmar@trootech.com>
2022-08-01 12:20:39 +0530

    CORS

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://62e761aa07f3246f7884acb6--meek-bunny-7dc9bb.netlify.app",
+  origin: "https://62e772f667682679294bccb0--meek-bunny-7dc9bb.netlify.app",
   credentials: true,
 };
 
